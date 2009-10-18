6d9a7f4a5769c57d2827863ea6d7300bdffd2116
Unknown Perforce User <pshao>
2009-10-18 18:18:34 -0500

    bug: none
    integrate change 215241 from main to FRANKLIN 
    ------------------------------------------------------
    Change 215241 by pshao@pshao-mac on 2009/10/18 16:17:32
    
            bug: none
    
            fixed zmprov gdpa usage.
    
    Copied from Perforce
     Change: 215242

--- src/db/sqlite/init.sql
+++
@@ -1,6 +0,0 @@
-.bail ON
-.read "@ZIMBRA_INSTALL@db/db.sql"
-.read "@ZIMBRA_INSTALL@db/wildfire.sql"
-.read "@ZIMBRA_INSTALL@db/versions-init.sql"
-.read "@ZIMBRA_INSTALL@db/default-volumes.sql"
-.exit
