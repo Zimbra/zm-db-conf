8f1b48ac17d07e09c1230419da3c6ef81816e27d
Quanah Gibson-Mount <quanah@zimbra.com>
2011-04-18 15:46:59 -0500

    Integrated the following from //depot/zimbra/main/...: Change 295324 by quanah@qmain on 2011/04/18 13:46:17
    bug: 59101
    If JYTHONPATH is not set, set it.  Affects being run via cron
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59101
    
    Copied from Perforce
     Change: 295325

--- src/db/sqlite/db.sql
+++ src/db/sqlite/db.sql
@@ -135,17 +135,7 @@ CREATE TABLE mobile_devices (
    remote_wipe_req     INTEGER UNSIGNED,
    remote_wipe_ack     INTEGER UNSIGNED,
    policy_values       VARCHAR(512),
-   last_used_date      DATE,
-   deleted_by_user     BOOLEAN NOT NULL DEFAULT 0,
-   model               VARCHAR(64),
-   imei                VARCHAR(64),
-   friendly_name       VARCHAR(512),
-   os                  VARCHAR(64),
-   os_language         VARCHAR(64),
-   phone_number        VARCHAR(64),
 
    PRIMARY KEY (mailbox_id, device_id),
    CONSTRAINT fk_mobile_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES mailbox(id) ON DELETE CASCADE
 );
-
-CREATE INDEX i_mobile_devices_last_used_date ON mobile_devices(last_used_date);
