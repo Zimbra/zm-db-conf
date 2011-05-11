46461c4603e2513efd9d3ecbb9e52120be872533
Unknown Perforce User <jhahm>
2011-05-11 17:14:46 -0500

    bug: 59044
    Integrating from main to GNR
    
    Change 307638 by jhahm@jhahm-mac3 on 2011/05/11 15:00:24
    
            bug: 59044
    
            Added debug logging around session refresh and notification sequence 
            changes.  zimbra.session logger at DEBUG level will log all changes 
            to SoapSession.mForceRefresh field, both before and after values.  
            The log will also indicate when refresh block is returned for a 
            session and show the notification sequence values used to determine 
            that refresh is needed.
    
            Example of mForceRefresh change logging:
    
            "handleRemoteNotifications: changing mForceRefresh: x -> y"
    
            Example of refresh decision logging:
    
            "refresh required: mForceRefresh=x, lastSequence=y, 
            currentSequence=z"
            "returning refresh block; reason=sequence-based"
    
            Refresh block can also be returned when a new session is created.  
            This case is logged too:
    
            "returning refresh block; reason=new session"
    
            Hopefully these logs will help us narrow down the cause for refresh 
            block being returned repeatedly with increasing notification 
            sequence number.  One possible way to reproduce the problem is by 
            introducing a large jump in mForceRefresh.  Client and server will 
            go into a loop with notification sequence going up by one until it 
            reaches mForceRefresh.  The extra logging will help us track down 
            where the big jump is coming from.  (We don't know this is what 
            happened.  A big jump in mForceRefresh is just one way to reproduce 
            the observed problem.)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59044
    
    Copied from Perforce
     Change: 307644

--- src/db/mysql/create_database.sql
+++ src/db/mysql/create_database.sql
@@ -1,30 +1,19 @@
---
+-- 
 -- ***** BEGIN LICENSE BLOCK *****
 -- Zimbra Collaboration Suite Server
--- Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
---
+-- Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+-- 
 -- The contents of this file are subject to the Zimbra Public License
 -- Version 1.3 ("License"); you may not use this file except in
 -- compliance with the License.  You may obtain a copy of the License at
 -- http://www.zimbra.com/license.
---
+-- 
 -- Software distributed under the License is distributed on an "AS IS"
 -- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 -- ***** END LICENSE BLOCK *****
---
+-- 
 CREATE DATABASE ${DATABASE_NAME}
 DEFAULT CHARACTER SET utf8;
-
-CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_address (
-   mailbox_id    INTEGER UNSIGNED NOT NULL,
-   id            INTEGER UNSIGNED NOT NULL,
-   address       VARCHAR(128) NOT NULL,
-   contact_count INTEGER NOT NULL,
-
-   PRIMARY KEY (mailbox_id, id),
-   UNIQUE INDEX i_mail_address_address (mailbox_id, address),
-   CONSTRAINT fk_mail_address_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id) ON DELETE CASCADE
-) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item (
    mailbox_id    INTEGER UNSIGNED NOT NULL,
@@ -42,8 +31,6 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item (
    flags         INTEGER NOT NULL DEFAULT 0,
    tags          BIGINT NOT NULL DEFAULT 0,
    sender        VARCHAR(128),
-   sender_id     INTEGER UNSIGNED DEFAULT NULL,
-   recipients    VARCHAR(128),
    subject       TEXT,
    name          VARCHAR(128),               -- namespace entry for item (e.g. tag name, folder name, document filename)
    metadata      MEDIUMTEXT,
@@ -70,52 +57,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item (
    CONSTRAINT fk_mail_item_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
    CONSTRAINT fk_mail_item_parent_id FOREIGN KEY (mailbox_id, parent_id) REFERENCES ${DATABASE_NAME}.mail_item(mailbox_id, id),
    CONSTRAINT fk_mail_item_folder_id FOREIGN KEY (mailbox_id, folder_id) REFERENCES ${DATABASE_NAME}.mail_item(mailbox_id, id),
-   CONSTRAINT fk_mail_item_sender_id FOREIGN KEY (mailbox_id, sender_id) REFERENCES ${DATABASE_NAME}.mail_address(mailbox_id, id),
    CONSTRAINT fk_mail_item_volume_id FOREIGN KEY (volume_id) REFERENCES zimbra.volume(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item_dumpster (
-   mailbox_id    INTEGER UNSIGNED NOT NULL,
-   id            INTEGER UNSIGNED NOT NULL,
-   type          TINYINT NOT NULL,           -- 1 = folder, 3 = tag, etc.
-   parent_id     INTEGER UNSIGNED,
-   folder_id     INTEGER UNSIGNED,
-   index_id      INTEGER UNSIGNED,
-   imap_id       INTEGER UNSIGNED,
-   date          INTEGER UNSIGNED NOT NULL,  -- stored as a UNIX-style timestamp
-   size          BIGINT UNSIGNED NOT NULL,
-   volume_id     TINYINT UNSIGNED,
-   blob_digest   VARCHAR(28) BINARY,         -- reference to blob, meaningful for messages only (type == 5)
-   unread        INTEGER UNSIGNED,           -- stored separately from the other flags so we can index it
-   flags         INTEGER NOT NULL DEFAULT 0,
-   tags          BIGINT NOT NULL DEFAULT 0,
-   sender        VARCHAR(128),
-   subject       TEXT,
-   recipients    VARCHAR(128),
-   name          VARCHAR(128),               -- namespace entry for item (e.g. tag name, folder name, document filename)
-   metadata      MEDIUMTEXT,
-   mod_metadata  INTEGER UNSIGNED NOT NULL,  -- change number for last row modification
-   change_date   INTEGER UNSIGNED,           -- UNIX-style timestamp for last row modification
-   mod_content   INTEGER UNSIGNED NOT NULL,  -- change number for last change to "content" (e.g. blob)
-
-   PRIMARY KEY (mailbox_id, id),
-   INDEX i_type (mailbox_id, type),          -- for looking up folders and tags
-   INDEX i_parent_id (mailbox_id, parent_id),-- for looking up a parent\'s children
-   INDEX i_folder_id_date (mailbox_id, folder_id, date), -- for looking up by folder and sorting by date
-   INDEX i_index_id (mailbox_id, index_id),  -- for looking up based on search results
-   INDEX i_unread (mailbox_id, unread),      -- there should be a small number of items with unread=TRUE
-                                             -- no compound index on (unread, date), so we save space at
-                                             -- the expense of sorting a small number of rows
-   INDEX i_date (mailbox_id, date),          -- fallback index in case other constraints are not specified
-   INDEX i_mod_metadata (mailbox_id, mod_metadata),      -- used by the sync code
-   INDEX i_tags_date (mailbox_id, tags, date),           -- for tag searches
-   INDEX i_flags_date (mailbox_id, flags, date),         -- for flag searches
-   INDEX i_volume_id (mailbox_id, volume_id),            -- for the foreign key into the volume table
-
-   -- Must not enforce unique index on (mailbox_id, folder_id, name) for the dumpster version!
-
-   CONSTRAINT fk_mail_item_dumpster_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
-   CONSTRAINT fk_mail_item_dumpster_volume_id FOREIGN KEY (volume_id) REFERENCES zimbra.volume(id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.revision (
@@ -136,26 +78,6 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.revision (
 
    CONSTRAINT fk_revision_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
    CONSTRAINT fk_revision_item_id FOREIGN KEY (mailbox_id, item_id) REFERENCES ${DATABASE_NAME}.mail_item(mailbox_id, id) ON DELETE CASCADE
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.revision_dumpster (
-   mailbox_id    INTEGER UNSIGNED NOT NULL,
-   item_id       INTEGER UNSIGNED NOT NULL,
-   version       INTEGER UNSIGNED NOT NULL,
-   date          INTEGER UNSIGNED NOT NULL,  -- stored as a UNIX-style timestamp
-   size          BIGINT UNSIGNED NOT NULL,
-   volume_id     TINYINT UNSIGNED,
-   blob_digest   VARCHAR(28) BINARY,         -- reference to blob, meaningful for messages only (type == 5)
-   name          VARCHAR(128),               -- namespace entry for item (e.g. tag name, folder name, document filename)
-   metadata      MEDIUMTEXT,
-   mod_metadata  INTEGER UNSIGNED NOT NULL,  -- change number for last row modification
-   change_date   INTEGER UNSIGNED,           -- UNIX-style timestamp for last row modification
-   mod_content   INTEGER UNSIGNED NOT NULL,  -- change number for last change to "content" (e.g. blob)
-
-   PRIMARY KEY (mailbox_id, item_id, version),
-
-   CONSTRAINT fk_revision_dumpster_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
-   CONSTRAINT fk_revision_dumpster_item_id FOREIGN KEY (mailbox_id, item_id) REFERENCES ${DATABASE_NAME}.mail_item_dumpster(mailbox_id, id) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.open_conversation (
@@ -183,20 +105,6 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.appointment (
 
 CREATE UNIQUE INDEX i_item_id ON ${DATABASE_NAME}.appointment (mailbox_id, item_id);
 
-CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.appointment_dumpster (
-   mailbox_id  INTEGER UNSIGNED NOT NULL,
-   uid         VARCHAR(255) NOT NULL,
-   item_id     INTEGER UNSIGNED NOT NULL,
-   start_time  DATETIME NOT NULL,
-   end_time    DATETIME,
-
-   PRIMARY KEY (mailbox_id, uid),
-   CONSTRAINT fk_appointment_dumpster_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
-   CONSTRAINT fk_appointment_dumpster_item_id FOREIGN KEY (mailbox_id, item_id) REFERENCES ${DATABASE_NAME}.mail_item_dumpster(mailbox_id, id) ON DELETE CASCADE
-) ENGINE = InnoDB;
-
-CREATE UNIQUE INDEX i_item_id ON ${DATABASE_NAME}.appointment_dumpster (mailbox_id, item_id);
-
 CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.tombstone (
    mailbox_id  INTEGER UNSIGNED NOT NULL,
    sequence    INTEGER UNSIGNED NOT NULL,  -- change number for deletion
@@ -214,7 +122,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.pop3_message (
    data_source_id CHAR(36) NOT NULL,
    uid            VARCHAR(255) BINARY NOT NULL,
    item_id        INTEGER UNSIGNED NOT NULL,
-
+   
    PRIMARY KEY (mailbox_id, item_id),
    CONSTRAINT fk_pop3_message_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id)
 ) ENGINE = InnoDB;
@@ -247,7 +155,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.imap_message (
    uid            BIGINT NOT NULL,
    item_id        INTEGER UNSIGNED NOT NULL,
    flags          INTEGER NOT NULL DEFAULT 0,
-
+   
    PRIMARY KEY (mailbox_id, item_id),
    CONSTRAINT fk_imap_message_mailbox_id FOREIGN KEY (mailbox_id)
       REFERENCES zimbra.mailbox(id) ON DELETE CASCADE,
@@ -265,7 +173,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.data_source_item (
    folder_id      INTEGER UNSIGNED NOT NULL DEFAULT 0,
    remote_id      VARCHAR(255) BINARY NOT NULL,
    metadata       MEDIUMTEXT,
-
+   
    PRIMARY KEY (mailbox_id, item_id),
    UNIQUE INDEX i_remote_id (mailbox_id, data_source_id, remote_id),   -- for reverse lookup
    CONSTRAINT fk_data_source_item_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id) ON DELETE CASCADE
