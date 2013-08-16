7fec2f3264d45d4a53940073d8381d52a30190e4
Quanah Gibson-Mount <quanah@zimbra.com>
2013-08-16 13:49:47 -0500

    Integrated the following from //depot/zimbra/main/...: Change 480457 by quanah@qmain on 2013/08/16 11:49:34
    bug: 83657
    Facebook spam rule
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83657
    
    Copied from Perforce
     Change: 480458

--- src/db/mysql/db.sql
+++ src/db/mysql/db.sql
@@ -1,7 +1,7 @@
 --
 -- ***** BEGIN LICENSE BLOCK *****
 -- Zimbra Collaboration Suite Server
--- Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
+-- Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
 -- 
 -- The contents of this file are subject to the Zimbra Public License
 -- Version 1.3 ("License"); you may not use this file except in
@@ -120,7 +120,6 @@ CREATE TABLE mailbox (
    highest_indexed     VARCHAR(21), -- deprecated
    version             VARCHAR(16),
    last_purge_at       INTEGER UNSIGNED NOT NULL DEFAULT 0,
-   itemcache_checkpoint       INTEGER UNSIGNED NOT NULL DEFAULT 0,
 
    UNIQUE INDEX i_account_id (account_id),
    INDEX i_index_volume_id (index_volume_id),
@@ -257,11 +256,4 @@ CREATE TABLE pending_acl_push (
    CONSTRAINT fk_pending_acl_push_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES mailbox(id) ON DELETE CASCADE,
    INDEX i_date (date)
 ) ENGINE = InnoDB;
-
-CREATE TABLE current_sessions (
-	id				INTEGER UNSIGNED NOT NULL,
-	server_id		VARCHAR(127) NOT NULL,
-	PRIMARY KEY (id, server_id)
-) ENGINE = InnoDB; 
-
 
