99bfdcdfa18f64f93b08683749c1c5d7647f0a89
Tony Publiski <tpubliski@zimbra.com>
2014-05-09 12:37:54 -0500

    Integrated the following from //depot/zimbra/IRONMAIDEN/...: Change 511124 by tpubliski@katbert on 2014/05/09 12:35:03
    Bug: 90032
    
    Stop attempting to perform mailboxd-related tasks if we cannot determine the pid.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=90032
    
    Copied from Perforce
     Change: 511128

--- src/db/migration/migrate20140319-MailItemPrevFolders.pl
+++ src/db/migration/migrate20140319-MailItemPrevFolders.pl
@@ -2,7 +2,7 @@
 #
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2011, 2013 Zimbra Software, LLC.
+# Copyright (C) 2014 Zimbra Software, LLC.
 # 
 # The contents of this file are subject to the Zimbra Public License
 # Version 1.4 ("License"); you may not use this file except in
@@ -16,36 +16,36 @@
 
 use strict;
 use Migrate;
-my $concurrent = 10;
+
 ########################################################################################################################
 
-Migrate::verifySchemaVersion(81);
+Migrate::verifySchemaVersion(100);
 
-addRecipientsColumn();
+foreach my $group (Migrate::getMailboxGroups()) {
+    addPrevFoldersColumnToMailItem($group);
+    addPrevFoldersColumnToMailItemDumpster($group);
+}
 
-Migrate::updateSchemaVersion(81, 82);
+Migrate::updateSchemaVersion(100, 101);
 
 exit(0);
 
 ########################################################################################################################
 
-sub addRecipientsColumn() {
-  my @groups = Migrate::getMailboxGroups();
-  my @sql = ();
-  foreach my $group (@groups) {
+sub addPrevFoldersColumnToMailItem($) {
+    my ($group) = @_;
+    Migrate::logSql("Adding prev_folder_ids column to mail_item table...");
     my $sql = <<_EOF_;
-ALTER TABLE $group.mail_item ADD COLUMN recipients VARCHAR(128) AFTER sender;
+ALTER TABLE $group.mail_item ADD COLUMN prev_folders TEXT AFTER folder_id;
 _EOF_
-    push(@sql,$sql);
-  }
-  Migrate::runSqlParallel($concurrent,@sql);
+  Migrate::runSql($sql);
+}
 
-  @sql = ();
-  foreach my $group (@groups) {
+sub addPrevFoldersColumnToMailItemDumpster($) {
+    my ($group) = @_;
+    Migrate::logSql("Adding prev_folder_ids column to mail_item_dumpster table...");
     my $sql = <<_EOF_;
-ALTER TABLE $group.mail_item_dumpster ADD COLUMN recipients VARCHAR(128) AFTER sender;
+ALTER TABLE $group.mail_item_dumpster ADD COLUMN prev_folders TEXT AFTER folder_id;
 _EOF_
-    push(@sql,$sql);
-  }
-  Migrate::runSqlParallel($concurrent,@sql);
+  Migrate::runSql($sql);
 }
---
+++ src/db/migration/migrate20140328-EnforceTableCharset.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Zimbra Collaboration Suite Server
+# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2013 Zimbra Software, LLC.
+# 
+# The contents of this file are subject to the Zimbra Public License
+# Version 1.4 ("License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://www.zimbra.com/license.
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# ***** END LICENSE BLOCK *****
+# 
+
+use strict;
+use Migrate;
+
+Migrate::verifySchemaVersion(101);
+enforceCharset();
+Migrate::updateSchemaVersion(101, 102);
+exit(0);
+
+sub enforceCharset()
+{
+  my @tables = ('mail_item_dumpster', 'revision_dumpster', 'appointment_dumpster');
+  foreach my $group (Migrate::getMailboxGroups()) {
+    foreach my $table (@tables) {
+    my $sql;
+    $sql = <<_EOF_;
+delimiter //
+create procedure convertCharset()
+    BEGIN
+    SET \@curCharset = 
+    (SELECT CCSA.character_set_name FROM information_schema.tables T,
+       information_schema.collation_character_set_applicability CCSA
+    WHERE CCSA.collation_name = T.table_collation
+    AND T.table_schema = "$group"
+    AND T.table_name = "$table");
+    IF \@curCharset!="utf8" 
+    THEN 
+        ALTER TABLE $group.$table
+        CONVERT TO CHARACTER SET "utf8";
+    END IF;
+    END//
+delimiter ;
+call convertCharset();
+drop procedure convertCharset;
+_EOF_
+ Migrate::runSql($sql);
+}
+}
+}
+
