b3383400c5e4c8c9b160fdfd4afeb6d763af53f1
Jyotiranjan Jena <jrjena136@gmail.com>
2020-07-01 10:52:35 +0530

    zcs-9354:Support for DeviceInformation (#10)
    (cherry picked from commit 0fd079e766e25dcc9474d26cdfd2cf8120723d79)
    (cherry picked from commit 77348667bf3f566f000069571377e7b8cae44e30)

--- src/db/mysql/db.sql
+++ src/db/mysql/db.sql
@@ -243,7 +243,8 @@ CREATE TABLE mobile_devices (
    phone_number        VARCHAR(64),
    unapproved_appl_list TEXT NULL,
    approved_appl_list   TEXT NULL,
-   
+   mobile_operator      VARCHAR(512),
+
    PRIMARY KEY (mailbox_id, device_id),
    CONSTRAINT fk_mobile_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES mailbox(id) ON DELETE CASCADE,
    INDEX i_last_used_date (last_used_date)
