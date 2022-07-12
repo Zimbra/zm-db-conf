8e7b463ad54ad17e7b6ff2ae63fa99e45e81dc0a
Indraneel Patil <indraneel.patil@synacor.com>
2022-07-12 15:15:50 +0530

    ZCS-11697 StoreType column should get created while installing fresh build

--- src/db/mysql/db.sql
+++ src/db/mysql/db.sql
@@ -50,6 +50,7 @@ CREATE TABLE volume (
    compress_blobs         BOOLEAN NOT NULL,
    compression_threshold  BIGINT NOT NULL,
    metadata               MEDIUMTEXT,
+   store_type             TINYINT(1) NOT NULL DEFAULT 1, -- 1 for onstore and 2 for s3 bucket
 
    UNIQUE INDEX i_name (name),
    UNIQUE INDEX i_path (path(255))   -- Index prefix length of 255 is the max prior to MySQL 4.1.2.  Should be good enough.
